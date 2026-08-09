7b7734520708c9e91bbd86d117487ace716f182b
Shuntatsu <148511409+shuntatsu@users.noreply.github.com>
2026-08-10 00:11:46 +0900

    feat: export microstructure calibration API

--- src/grid_trade/calibration/__init__.py
+++ src/grid_trade/calibration/__init__.py
@@ -10,6 +10,12 @@ from grid_trade.calibration.engine import (
     CalibrationUpdate,
     update_calibration_engine,
 )
+from grid_trade.calibration.execution_cost import (
+    ExecutionCostConfig,
+    ExecutionCostEstimate,
+    estimate_execution_cost,
+    relative_adverse_markout,
+)
 from grid_trade.calibration.funding import (
     FundingCalibrationConfig,
     FundingCalibrationState,
@@ -29,11 +35,24 @@ from grid_trade.calibration.microstructure_contracts import (
     OfiImpactSample,
     TopOfBookObservation,
 )
+from grid_trade.calibration.microstructure_engine import (
+    MicrostructureCalibrationConfig,
+    MicrostructureCalibrationEstimate,
+    MicrostructureCalibrationState,
+    MicrostructureCalibrationUpdate,
+    update_microstructure_engine,
+)
 from grid_trade.calibration.order_flow import (
+    OfiImpactConfig,
+    OfiImpactEstimate,
+    OfiImpactState,
     compute_ofi,
+    estimate_ofi_impact,
     microprice,
     microprice_displacement,
     normalized_ofi,
+    predict_ofi_displacement,
+    update_ofi_impact,
 )
 from grid_trade.calibration.trend import (
     TrendCalibrationConfig,
@@ -55,6 +74,8 @@ __all__ = [
     "CalibrationObservation",
     "CalibrationReadiness",
     "CalibrationUpdate",
+    "ExecutionCostConfig",
+    "ExecutionCostEstimate",
     "FundingCalibrationConfig",
     "FundingCalibrationState",
     "FundingEstimate",
@@ -63,8 +84,15 @@ __all__ = [
     "IntensityEstimate",
     "MarkoutSide",
     "MaturedMarkout",
+    "MicrostructureCalibrationConfig",
+    "MicrostructureCalibrationEstimate",
+    "MicrostructureCalibrationState",
+    "MicrostructureCalibrationUpdate",
     "MicrostructureReadiness",
+    "OfiImpactConfig",
+    "OfiImpactEstimate",
     "OfiImpactSample",
+    "OfiImpactState",
     "RobustVolatilityConfig",
     "RobustVolatilityState",
     "TopOfBookObservation",
@@ -73,11 +101,17 @@ __all__ = [
     "VolatilityEstimate",
     "compute_ofi",
     "estimate_arrival_intensity",
+    "estimate_execution_cost",
     "estimate_normalized_trend",
+    "estimate_ofi_impact",
     "microprice",
     "microprice_displacement",
     "normalized_ofi",
+    "predict_ofi_displacement",
+    "relative_adverse_markout",
     "update_calibration_engine",
     "update_funding_calibration",
+    "update_microstructure_engine",
+    "update_ofi_impact",
     "update_robust_volatility",
 ]
