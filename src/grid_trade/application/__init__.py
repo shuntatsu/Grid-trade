2c9cdf7259b0d5c248924bb711d171e11149db9f
Shuntatsu <148511409+shuntatsu@users.noreply.github.com>
2026-08-09 20:44:11 +0900

    feat: export adaptive grid application API

--- src/grid_trade/application/__init__.py
+++ src/grid_trade/application/__init__.py
@@ -1,3 +1,8 @@
+from grid_trade.application.adaptive_grid import (
+    AdaptiveGridTransition,
+    continue_adaptive_grid_reconciliation,
+    transition_adaptive_grid,
+)
 from grid_trade.application.dynamic_center import (
     DynamicCenterTransition,
     continue_dynamic_center_reconciliation,
@@ -15,12 +20,15 @@ from grid_trade.application.s2_adaptive_grid import (
 )
 
 __all__ = [
+    "AdaptiveGridTransition",
     "DynamicCenterTransition",
     "PassivePolicyTransition",
     "S2AdaptiveGridTransition",
+    "continue_adaptive_grid_reconciliation",
     "continue_dynamic_center_reconciliation",
     "continue_passive_policy_reconciliation",
     "continue_s2_adaptive_grid_reconciliation",
+    "transition_adaptive_grid",
     "transition_dynamic_center",
     "transition_passive_policy",
     "transition_s2_adaptive_grid",
