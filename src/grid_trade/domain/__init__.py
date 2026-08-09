f128ea194b0590c30332e8700e5d5dd34accde43
Shuntatsu <148511409+shuntatsu@users.noreply.github.com>
2026-08-09 16:14:39 +0900

    feat: expose grid domain contracts

---
+++ src/grid_trade/domain/__init__.py
@@ -0,0 +1,19 @@
+from grid_trade.domain.market import MarketSnapshot
+from grid_trade.domain.orders import (
+    FillEvent,
+    OrderAction,
+    OrderSide,
+    PassiveOrderIntent,
+    ReconciliationPlan,
+    WorkingOrder,
+)
+
+__all__ = [
+    "FillEvent",
+    "MarketSnapshot",
+    "OrderAction",
+    "OrderSide",
+    "PassiveOrderIntent",
+    "ReconciliationPlan",
+    "WorkingOrder",
+]
