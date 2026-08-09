8e9faaede6ce53cbaf1fb8db448f6947abc7bf9d
Shuntatsu <148511409+shuntatsu@users.noreply.github.com>
2026-08-09 16:35:23 +0900

    feat: expose microstructure replay adapter

---
+++ src/grid_trade/research/__init__.py
@@ -0,0 +1,17 @@
+from grid_trade.research.hftbacktest_adapter import (
+    HftReplayConfig,
+    ReplayFill,
+    ReplaySummary,
+    load_microstructure_fixture,
+    replay_passive_orders,
+    require_hftbacktest_runtime,
+)
+
+__all__ = [
+    "HftReplayConfig",
+    "ReplayFill",
+    "ReplaySummary",
+    "load_microstructure_fixture",
+    "replay_passive_orders",
+    "require_hftbacktest_runtime",
+]
