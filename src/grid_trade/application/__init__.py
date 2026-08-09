add27c453ba64c0d3afd0a3d552148d22b28bbbe
Shuntatsu <148511409+shuntatsu@users.noreply.github.com>
2026-08-10 03:52:56 +0900

    feat: export calibrated adaptive application API

--- src/grid_trade/application/__init__.py
+++ src/grid_trade/application/__init__.py
@@ -3,6 +3,18 @@ from grid_trade.application.adaptive_grid import (
     continue_adaptive_grid_reconciliation,
     transition_adaptive_grid,
 )
+from grid_trade.application.calibrated_adaptive import (
+    CalibratedAdaptiveInputs,
+    CalibratedAdaptiveMetaConfig,
+    CalibratedAdaptivePreparation,
+    CalibratedAdaptiveState,
+    CalibratedAdaptiveTransition,
+    VenueGridConstraints,
+    continue_calibrated_adaptive_reconciliation,
+    initialize_calibrated_adaptive_grid,
+    prepare_calibrated_adaptive_inputs,
+    transition_calibrated_adaptive_grid,
+)
 from grid_trade.application.dynamic_center import (
     DynamicCenterTransition,
     continue_dynamic_center_reconciliation,
@@ -21,14 +33,24 @@ from grid_trade.application.s2_adaptive_grid import (
 
 __all__ = [
     "AdaptiveGridTransition",
+    "CalibratedAdaptiveInputs",
+    "CalibratedAdaptiveMetaConfig",
+    "CalibratedAdaptivePreparation",
+    "CalibratedAdaptiveState",
+    "CalibratedAdaptiveTransition",
     "DynamicCenterTransition",
     "PassivePolicyTransition",
     "S2AdaptiveGridTransition",
+    "VenueGridConstraints",
     "continue_adaptive_grid_reconciliation",
+    "continue_calibrated_adaptive_reconciliation",
     "continue_dynamic_center_reconciliation",
     "continue_passive_policy_reconciliation",
     "continue_s2_adaptive_grid_reconciliation",
+    "initialize_calibrated_adaptive_grid",
+    "prepare_calibrated_adaptive_inputs",
     "transition_adaptive_grid",
+    "transition_calibrated_adaptive_grid",
     "transition_dynamic_center",
     "transition_passive_policy",
     "transition_s2_adaptive_grid",
